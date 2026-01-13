3179096f79cb4d72dd3649ea629654816e61ae11
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2026-01-13 03:01:07 +0100

    🚧WIP: teneancy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TenancyModule } from './tenancy/tenancy.module';
+import { TenancyMiddleware } from './tenancy/tenancy.middleware';
 
 @Module({
-  imports: [],
+  imports: [TenancyModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TenancyMiddleware).forRoutes('*');
+  }
+}
---
+++ src/tenancy/tenancy.service.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+import { Tenants } from './tenants.interface';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+@Injectable()
+export class TenancyService implements OnModuleInit {
+  private tenants: Tenants;
+
+  onModuleInit() {
+    const tenants: Tenants = JSON.parse(
+      readFileSync(join(__dirname, './tenants.json'), 'utf8'),
+    );
+
+    this.tenants = tenants;
+  }
+
+  validateTenantId(tenantId: string) {
+    if (!tenantId || !this.tenants[tenantId]) {
+      throw new BadRequestException(`Tenant ${tenantId} not found`);
+    }
+  }
+}
